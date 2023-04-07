cd67800070a45546d2d82b9f1d367b9df3e0ac7a
Nico Rittinghaus <nico.rittinghaus@hhu.de>
2023-04-07 19:58:41 +0200

    improved basic test

--- newick/backend/test/test_node.py
+++ newick/backend/test/test_node.py
@@ -1,6 +1,19 @@
 from newick.backend.node import Node
 
 
-def test_simple_instantiation():
+def test_simple_instantiation_and_parenting():
+    # label only
     node0 = Node("testa")
     assert node0.get_label() == "testa"
+    # label and distance
+    node1 = Node("testb", distance=2.25)
+    assert node1.get_label() == "testb"
+    assert node1.get_distance() == 2.25
+    # now some parenting
+    node0.add_child(node1)
+    assert node0.count_children() == 1
+    assert node1.count_children() == 0
+    assert node0.get_child_by_label("testa") is node1
+    assert node0.get_child_by_label("xyz") == None
+    
+#def test_
