0acd164831c8e2fa00ffdffab4fcacc8a7121327
Nico Rittinghaus <nico.rittinghaus@hhu.de>
2023-04-14 12:44:54 +0200

    found a bug and changed behaviour of distance adjustments and duplication counts so that those are only performed when explicitly stated in path

--- newick/backend/test/test_tree.py
+++ newick/backend/test/test_tree.py
@@ -15,10 +15,22 @@ def test_partial_duplicate():
     t.add_new_node(Path("R", [("A", 1.0)]))
     t.add_new_node(Path("R", [("B", 4.0)]))
     t.add_new_node(Path("R", [("B", 2.0), ("C-c", 2.2)]))
-    assert t.to_string() == "(A:1,(C-c:2.2)B:3)R;"
+    assert t.to_string() == "(A:1,(C-c:2.2)B:3)R;" # note: dist of B changed
     assert t._root.get_child_by_label("B").get_duplication_count() == 0
     t.add_new_node(Path("R", [("B", 4.0)]))
-    assert t.to_string() == "(A:1,(C-c:2.2)B:3.5)R;"
+    assert t.to_string() == "(A:1,(C-c:2.2)B:3.5)R;" # note: changed again
+    assert t._root.get_child_by_label("B").get_duplication_count() == 1
+    
+def test_partial_duplicate_no_dist_def():
+    r = Tree.RootNode("R")
+    t = Tree(r)
+    t.add_new_node(Path("R", [("A", 1.0)]))
+    t.add_new_node(Path("R", [("B", 4.0)]))
+    t.add_new_node(Path("R", [("B", float("-inf")), ("C-c", 2.2)]))
+    assert t.to_string() == "(A:1,(C-c:2.2)B:4)R;" # note: dist of B changed
+    assert t._root.get_child_by_label("B").get_duplication_count() == 0
+    t.add_new_node(Path("R", [("B", float("-inf"))]))
+    assert t.to_string() == "(A:1,(C-c:2.2)B:4)R;" # note: changed again
     assert t._root.get_child_by_label("B").get_duplication_count() == 1
     
 def test_exact_duplicate():
@@ -30,3 +42,5 @@ def test_exact_duplicate():
     assert t.to_string() == "(A:1,(C-c:2.2)B:3)R;"
     assert t._root.get_child_by_label("B").get_duplication_count() == 0
     assert t._root.get_child_by_label("B").get_child_by_label("C-c").get_duplication_count() == 1
+    
+    
